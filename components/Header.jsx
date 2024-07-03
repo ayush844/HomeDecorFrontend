aec2bbc002db26a15ee5a77d18bfad8addced902
ayush844 <ayush.xyz1625@gmail.com>
2024-07-03 15:57:02 +0530

    build theme toggler
    Signed-off-by: ayush844 <ayush.xyz1625@gmail.com>

---
+++ components/Header.jsx
@@ -0,0 +1,14 @@
+"use client"
+
+import React from 'react'
+import ThemeToggler from './ThemeToggler'
+
+const Header = () => {
+  return (
+    <div>
+        <ThemeToggler />
+    </div>
+  )
+}
+
+export default Header
