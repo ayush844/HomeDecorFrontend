943a7a186109259db5e00ccc415e6fcf51907764
ayush844 <ayush.xyz1625@gmail.com>
2024-07-05 02:35:35 +0530

    mobile navigation and vertical progress
    Signed-off-by: ayush844 <ayush.xyz1625@gmail.com>

---
+++ app/template.jsx
@@ -0,0 +1,28 @@
+"use client"
+
+import React from 'react';
+import { motion } from 'framer-motion';
+
+import UseScrollbarProgress from '@/hooks/UseScrollbarProgress'
+
+const variants = {
+    hidden: {opacity: 0},
+    enter: {opacity: 1}
+}
+
+const template = ({children}) => {
+
+    const completion = UseScrollbarProgress();
+
+  return (
+    <>
+        <motion.main variants={variants} initial='hidden' animate='enter' transition={ {type: 'linear', delay: 0.2, duration: 0.4}}>
+            {children}
+        </motion.main>
+        <span style={{transform: `translateY(${completion-100}%)`}} className=' fixed z-50 bg-primary w-1 lg:w-2 top-0 right-0 bottom-0 transition-all duration-700'></span>
+        <div className="h-[2500px]"></div>
+    </>
+  )
+}
+
+export default template
--- components/Header.jsx
+++ components/Header.jsx
@@ -1,14 +1,32 @@
 "use client"
 
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import ThemeToggler from './ThemeToggler'
 import Logo from './Logo'
 import Nav from './Nav'
 import MobileNavigation from './MobileNavigation'
+import { usePathname } from 'next/navigation'
 
 const Header = () => {
+
+  const [header, setHeader] = useState(false);
+  const pathname = usePathname();
+
+  useEffect(()=>{
+    const scrollYPos = window.addEventListener('scroll', ()=>{
+      window.scrollY > 50 ? setHeader(true) : setHeader(false);
+    });
+
+
+
+    return ()=> window.removeEventListener('scroll', scrollYPos);
+  })
+
+
+
+
   return (
-    <header>
+    <header className={`${header ? 'py-3 bg-red-50 shadow-lg dark:bg-blue-950' : 'py-5 dark:bg-transparent'} sticky top-0 z-30 transition-all ${pathname==="/" && "bg-[#fff]"}`}>
       <div className=' container mx-0 sm:mx-auto py-0 sm:py-2'>
         <div className='flex items-center justify-between gap-2'>
           {/* LOGO */}
