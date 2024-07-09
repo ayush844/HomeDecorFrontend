2a236f6d94815b1577530e62bf42f353112d8c46
ayush844 <ayush.xyz1625@gmail.com>
2024-07-10 02:11:55 +0530

    gallery page
    Signed-off-by: ayush844 <ayush.xyz1625@gmail.com>

--- app/gallery/page.jsx
+++ app/gallery/page.jsx
@@ -1,8 +1,54 @@
-import React from 'react'
-
 const Gallery = () => {
   return (
-    <div>this is Gallery page</div>
+    <div className=' container py-10 grid grid-cols-2 md:grid-cols-4 gap-4'>
+      <div className=" flex flex-col gap-4">
+        <div>
+          <img src="/image/badroom.jpg" alt="" className=" h-auto max-w-full rounded-lg hover:scale-90 transition-all" />
+        </div>
+        <div>
+          <img src="/image/gallery2.jpg" alt="" className=" h-auto max-w-full rounded-lg hover:scale-90 transition-all" />
+        </div>
+        <div>
+          <img src="/image/gallery.jpg" alt="" className=" h-auto max-w-full rounded-lg hover:scale-90 transition-all" />
+        </div>
+      </div>
+
+      <div className=" flex flex-col gap-4">
+        <div>
+          <img src="/image/swiper1.jpg" alt="" className=" h-auto max-w-full rounded-lg hover:scale-90 transition-all" />
+        </div>
+        <div>
+          <img src="/image/gallery11.jpg" alt="" className=" h-auto max-w-full rounded-lg hover:scale-90 transition-all" />
+        </div>
+        <div>
+          <img src="/image/living.jpg" alt="" className=" h-auto max-w-full rounded-lg hover:scale-90 transition-all" />
+        </div>
+      </div>
+
+      <div className=" flex flex-col gap-4">
+        <div>
+          <img src="/image/gallery14.jpg" alt="" className=" h-auto max-w-full rounded-lg hover:scale-90 transition-all" />
+        </div>
+        <div>
+          <img src="/image/gallery15.jpg" alt="" className=" h-auto max-w-full rounded-lg hover:scale-90 transition-all" />
+        </div>
+        <div>
+          <img src="/image/gallery12.jpg" alt="" className=" h-auto max-w-full rounded-lg hover:scale-90 transition-all" />
+        </div>
+      </div>
+
+      <div className=" flex flex-col gap-4">
+        <div>
+          <img src="/image/gallery7.jpg" alt="" className=" h-auto max-w-full rounded-lg hover:scale-90 transition-all" />
+        </div>
+        <div>
+          <img src="/image/gallery11.jpg" alt="" className=" h-auto max-w-full rounded-lg hover:scale-90 transition-all" />
+        </div>
+        <div>
+          <img src="/image/gallery3.jpg" alt="" className=" h-auto max-w-full rounded-lg hover:scale-90 transition-all" />
+        </div>
+      </div>
+    </div>
   )
 }
 
